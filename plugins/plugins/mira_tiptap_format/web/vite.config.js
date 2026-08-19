6edebce0ca3fd19a3333be95464ac7477adb4631
hunmer <liaoyanjie2000@gmail.com>
2026-08-19 12:40:24 +0800

    fix

--- plugins/plugins/mira_tiptap_format/web/vite.config.js
+++
@@ -1,9 +0,0 @@
-import path from 'node:path';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-export default defineConfig({
-    plugins: [vue()],
-    resolve: { alias: { '@': path.resolve(__dirname, './src') } },
-    base: './',
-    build: { outDir: 'dist', emptyOutDir: true, target: 'chrome100' },
-});
